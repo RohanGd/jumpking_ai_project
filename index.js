fc77fa1ed288c69659fe6a79f31b176da6f066f6
RohanGd <rohangadgil28@gmail.com>
2022-09-05 13:02:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 canvas.width = 1200
 canvas.height = 900
-const gravity = 0.3
+const gravity = 0.7
 
 c.fillRect((window.innerWidth - canvas.width)/2, 0, canvas.width, canvas.height)
 
@@ -11,27 +11,24 @@ class Sprite {
         this.position = position
         this.width = size.width
         this.height = size.height
+        this.lastKey
         this.velocity = {
             x:0,
             y:0
         }
-        this.speed = {
-            x:5,
-            y:10
-        }
+        // this.speed = {
+        //     x:5,
+        //     y:10
+        // }
     }
 
     update() {
-        if (this.position.x <= (window.innerWidth - canvas.width)/2 && this.position.x >= canvas.width)
-            this.velocity.x = 0
-        else this.position.x += this.velocity.x
         this.draw()
-        
+        this.position.x += this.velocity.x
         this.position.y += this.velocity.y
         if(this.position.y < canvas.height - this.height - this.velocity.y) {
             this.velocity.y += gravity
-        }
-        else this.velocity.y = 0
+        } else this.velocity.y = 0
 
     }
 
@@ -42,20 +39,101 @@ class Sprite {
 }
 
 player = new Sprite({
-    x:100,
-    y:100
+    x: 600,
+    y: 800
 }, {
     width: 50,
     height: 100
 })
 
+const keys = {
+    a : {
+        pressed : false
+    },
+    d : {
+        pressed : false
+    },
+    space : {
+        pressed : false
+    }
+}
+
+// function delay(delayInms) {
+//     return new Promise(resolve => {
+//       setTimeout(() => {
+//         resolve(2);
+//       }, delayInms);
+//     });
+//   }
 
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = "black"
     c.fillRect((window.innerWidth - canvas.width)/2, 0, canvas.width, canvas.height)
     player.update()
+
+    // setTimeout(playMove(),1000)
+    player.velocity.x = 0
+    // player.velocity.y = 0
+    if (keys.a.pressed && player.lastKey === 'a' && player.position.x > (window.innerWidth - canvas.width)/2) {
+        // await delay(1000)
+        player.velocity.x = -10
+    } else if (keys.d.pressed && player.lastKey === 'd') {
+        // await delay(1000)
+        player.velocity.x = 10
+    }
+
+    if (keys.space.pressed) {
+        player.velocity.y  = -20
+    }
+
+    resolveCollision()
+    // resetKeys()
 }
+
+function resolveCollision() {
+    if ((player.position.x > (window.innerWidth - canvas.width) / 2 )
+    && (player.position.x < canvas.width - player.width)) return 
+    else {
+        player.velocity.x *= -0.75
+    }
+}
+// function genRandomKey() {
+//     let x = Math.floor(Math.random() * 3)   
+//     if (x === 0) return 'd'
+//     else if (x === 1) return 'a'
+//     else return ' '
+// }
+
+// function playMove() {
+//     let move = genRandomKey()
+//     console.log(move)
+    
+//     switch (move) {
+//         case ' ':
+//             keys.space.pressed = true
+//             break;
+//         case 'd':
+//             keys.d.pressed = true
+//             player.lastKey = 'd'
+//             break;
+//         case 'a':
+//             keys.a.pressed = true
+//             player.lastKey = 'a'
+//             break;
+//         default:
+//             break;
+//     }
+
+// }
+
+// function resetKeys() {
+//     // for(k in keys) {
+//     //     k.pressed = false
+//     // }
+
+//     keys.space.pressed = false
+// }
 
 player.draw()
 animate()
@@ -64,28 +142,32 @@ window.addEventListener('keydown', (event) => {
     console.log(event)
     switch (event.key) {
         case ' ':
-            player.velocity.y = -10
+            keys.space.pressed = true
             break;
         case 'd':
-            player.velocity.x = 2.5
+            keys.d.pressed = true
+            player.lastKey = 'd'
             break;
         case 'a':
-            player.velocity.x = -2.5
+            keys.a.pressed = true
+            player.lastKey = 'a'
             break;
         default:
             break;
     }
 })
 
-
 window.addEventListener('keyup', (event) => {
     console.log(event)
     switch (event.key) {
+        case ' ':
+            keys.space.pressed = false
+            break;
         case 'd':
-            player.velocity.x = 0
+            keys.d.pressed = false
             break;
         case 'a':
-            player.velocity.x = 0
+            keys.a.pressed = false
             break;
         default:
             break;
